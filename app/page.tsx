5485167ad6d2d358066fb324100a4c95ecfa00f0
Joseph Brendan <jbrendan86@gmail.com>
2026-02-10 23:13:15 +0100

    fix(build): refactor success page and force dynamic home

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default async function Home() {
     let session = null;
